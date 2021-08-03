30a439cb2da10a5c90335977f5bae2ccd50a28eb
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-08-03 15:59:59 +0300

    order

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        Beverage beverage = new Espresso();
-
-        System.out.println(beverage.getDescription() + " $" + beverage.cost());
-
-        beverage = new Soy(beverage);
-        beverage = new Mocha(beverage);
-        beverage = new Whip(beverage);
-
-        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+        System.out.println("Выберите напиток из предложенных");
+        System.out.println(list());
+        Scanner scanner = new Scanner(System.in);
+        String sss = scanner.nextLine();
+        Beverage beverage1 =drink(sss);
+        System.out.println("Выберите напиток из предложенных");
+        System.out.println(listComp());
+        String sss1 = scanner.nextLine();
+        beverage1 = drinkWithSupplement(sss1,beverage1);
+        // метод стаканчик и палочка
+        System.out.println(beverage1.getDescription() + " $" + beverage1.cost());
+    }
+    public static Beverage drinkWithSupplement(String sss1,Beverage beverage){
+        if (sss1.equalsIgnoreCase("Soy")){
+            return new Soy(beverage);
+        }
+        if (sss1.equalsIgnoreCase("Whip")){
+            return new Whip(beverage);
+        }
+        if (sss1.equalsIgnoreCase("Mocha")){
+            return new Mocha(beverage);
+        }
+        return null;
 
     }
+
+    public static Beverage drink(String sss){
+        if (sss.equalsIgnoreCase("Decaf")){
+            return new Decaf();
+        }
+        if (sss.equalsIgnoreCase("Espresso")){
+            return new Espresso();
+        }
+        if (sss.equalsIgnoreCase("DarkRoast")){
+            return new DarkRoast();
+        }
+        return null;
+    }
+
+    public static List<String> list() {
+        List<String> list = new ArrayList<>();
+        list.add("Espresso");
+        list.add("DarkRoast");
+        list.add("Decaf");
+        return list;
+    }
+    public static List<String> listComp() {
+        List<String> list = new ArrayList<>();
+        list.add("Soy");
+        list.add("Whip");
+        list.add("Mocha");
+        return list;
+    }
+
+
 }
--- src/Soy.java
+++ src/Soy.java
@@ -1,13 +1,24 @@
 public class Soy extends CondimentDecorator {
     Beverage beverage;
+    int soy;
 
     public Soy(Beverage beverage) {
         this.beverage = beverage;
     }
-    public String getDescription (){
+
+    public void setSoy(int soy) {
+        this.soy = soy;
+    }
+
+    public int add(int x) {
+        return this.soy + x;
+    }
+
+    public String getDescription() {
         return beverage.getDescription() + ", Soy";
     }
-    public double cost (){
+
+    public double cost() {
         return 0.40 + beverage.cost();
     }
 }
